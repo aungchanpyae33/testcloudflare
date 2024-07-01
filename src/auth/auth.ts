691f768d72468dffbee4c3c56fd6ad9e625460c2
aungchanpyae <khantlu24@gmail.com>
2024-07-01 16:51:40 +0630

    add logout function

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -3,7 +3,7 @@ import { adapter } from "./lucia";
 import { GitHub } from "arctic";
 import { cookies } from "next/headers";
 import { cache } from "react";
-
+import { redirect } from "next/navigation";
 import type { Session, User } from "lucia";
 
 export const github = new GitHub(
@@ -80,3 +80,27 @@ export const validateRequest = cache(
     return result;
   }
 );
+
+export async function logout(): Promise<ActionResult> {
+  "use server";
+  const { session } = await validateRequest();
+  if (!session) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+  return redirect("/login");
+}
+
+interface ActionResult {
+  error: string | null;
+}
