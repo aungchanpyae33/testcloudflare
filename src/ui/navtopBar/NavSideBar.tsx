545d3bb99737f7d67ff8cab7ebe6a8fc36929d19
aungchanpyae <khantlu24@gmail.com>
2024-07-18 18:45:10 +0630

    add overlay

--- src/ui/navtopBar/NavSideBar.tsx
+++ src/ui/navtopBar/NavSideBar.tsx
@@ -1,32 +1,69 @@
 "use client";
 import clsx from "clsx";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import NavSideLink from "./NavSideLink";
+
+import MenuItem from "./MenuItem";
+import OverLay from "./OverLay";
+import CloseFunctoion from "@/lib/CloseFunction";
 function NavSideBar() {
+  const DivRef = useRef<HTMLUListElement | null>(null);
   const [open, setopen] = useState(false);
+
+  // CloseFunctoion(open, setopen, DivRef);
   return (
     <div>
       <ul
+        ref={DivRef}
         className={clsx(
-          "absolute bottom-0 duration-500  text-center left-0  translate-y-[99.9%] h-[80svh]  flex flex-col items-center  rounded-b-sm bg-green-900",
+          "fixed top-0 z-50 box-border transition-[width] duration-300 text-center left-0 h-[100%] overflow-auto  flex flex-col bg-green-500  rounded-b-sm  ",
           {
-            "w-[10%]": open === false,
-            "w-[40%]": open === true,
+            "w-[50px]": open === false,
+            "w-[200px]": open === true,
           }
         )}
       >
-        <li
-          className="bg-blue-500"
-          onClick={() => {
-            setopen(!open);
-          }}
-        >
-          {open ? "close" : "open"}
+        <li className=" min-h-[50px] sticky top-0 z-50 flex items-center">
+          <p onClick={() => setopen(!open)} className=" w-[50px] z-50">
+            open
+          </p>
+          <MenuItem open={open}>
+            <Link href={"/"}>Bubble</Link>
+          </MenuItem>
         </li>
-        <Link href={"/search"}>Song</Link>
-        <Link href={"/three"}>three</Link>
-        <li>four</li>
-      </ul>
+        <NavSideLink
+          url={"/search"}
+          icon="search"
+          desp="search bar"
+          open={open}
+        />
+        <NavSideLink
+          url={"/search"}
+          icon="search"
+          desp="search bar"
+          open={open}
+        />
+        <NavSideLink
+          url={"/search"}
+          icon="search"
+          desp="search bar"
+          open={open}
+        />{" "}
+        <NavSideLink
+          url={"/search"}
+          icon="search"
+          desp="search bar"
+          open={open}
+        />{" "}
+        <NavSideLink
+          url={"/search"}
+          icon="search"
+          desp="search bar"
+          open={open}
+        />
+      </ul>{" "}
+      {open && <OverLay setopen={setopen} />}
     </div>
   );
 }
