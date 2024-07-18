1191274cd709ca4ce8eb4086e926e685d3d9ca38
aungchanpyae <khantlu24@gmail.com>
2024-07-18 18:44:20 +0630

    add  function to overlay

--- src/ui/navtopBar/OverLay.tsx
+++ src/ui/navtopBar/OverLay.tsx
@@ -1,6 +1,13 @@
-function OverLay() {
+function OverLay({
+  setopen,
+}: {
+  setopen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   return (
-    <div className="z-40 bg-overlay absolute top-0 left-0 bottom-0 right-0"></div>
+    <div
+      onClick={() => setopen(false)}
+      className="z-40 bg-overlay absolute top-0 left-0 bottom-0 right-0"
+    ></div>
   );
 }
 
