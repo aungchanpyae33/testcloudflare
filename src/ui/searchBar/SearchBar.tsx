d98acfd3e7b5d93ef65c7afd3886731a9ea02aad
aungchanpyae <khantlu24@gmail.com>
2024-07-20 13:06:23 +0630

    closesearchfunction when click outside search

--- src/ui/searchBar/SearchBar.tsx
+++ src/ui/searchBar/SearchBar.tsx
@@ -1,10 +1,13 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import SearchResult from "./SearchResult";
 import { Movie } from "@/database/data";
+import CloseFunctoion from "@/lib/CloseFunction";
 
 function SearchBar({ data }: { data: Movie[] }) {
+  const DivRef = useRef<HTMLDivElement | null>(null);
+  const [open, setopen] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -18,30 +21,35 @@ function SearchBar({ data }: { data: Movie[] }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }
+  console.log(open);
+  CloseFunctoion(open, setopen, DivRef);
   return (
-    <div className="SearchContainer w-[70%] z-10 mx-auto relative ">
-      <div className="  max-w-[600px] w-[100%] mx-auto ">
+    <div className="SearchContainer w-[98%] z-0 mx-auto ">
+      <div ref={DivRef} className="max-w-[600px] w-[100%] relative mx-auto ">
         <label className="">
           <span className="sr-only">Search</span>
           <input
-            className="placeholder:text-slate-400 block bg-white w-full   border border-slate-300  py-2 pl-9  shadow-sm focus:outline-none sm:text-sm"
+            className="placeholder:text-slate-400 block bg-blue w-full   border border-slate-300  py-2 pl-9  shadow-sm focus:outline-none sm:text-sm"
             placeholder="Search for song and artist"
             type="search"
             name="search"
             autoComplete="off"
             ref={inputRef}
+            defaultValue={searchParams.get("query")?.toString()}
             // prevent up and down for displaying search list
+            onFocus={() => setopen(true)}
             onKeyDown={(e) => {
               if (e.key === "ArrowUp") {
                 e.preventDefault();
               }
             }}
             onChange={(e) => {
+              setopen(true);
               handleSearch(e.target.value);
             }}
           />
+          {open && <SearchResult data={data} inputRef={inputRef} />}
         </label>
-        <SearchResult data={data} inputRef={inputRef} />
       </div>
     </div>
   );
