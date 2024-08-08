fc80c536443830ca873e559ef7acddb18234f1ac
aungchanpyae <khantlu24@gmail.com>
2024-08-08 17:30:17 +0630

    sql query v-0.1

--- src/database/data.ts
+++ src/database/data.ts
@@ -1,20 +1,56 @@
 import { turso } from "./turso";
 
 export interface Movie {
-  year: string;
+  title: string;
+
   // Assuming you have a unique id for each movie
 }
+export interface MovieRe {
+  title: string;
 
-export async function getData(query: string) {
+  // Assuming you have a unique id for each movie
+}
+export const getData = async (query: string) => {
   console.log(query);
   if (query.length > 0) {
     const { rows } = await turso.execute(
-      `SELECT year FROM movies WHERE year LIKE '${query}%' `
+      `SELECT title FROM movieB WHERE title LIKE '${query}%'`
     );
     const data: Movie[] = rows.map((row: any) => ({
-      year: row.year,
+      title: row.title,
     }));
+
     return data;
   }
+  return [];
+};
+
+export async function getRecom(query: string) {
+  console.log(query);
+
+  if (query.length > 0) {
+    console.time("Execution");
+
+    const { rows } = await turso.execute(`
+      WITH extracted_vector AS (
+        SELECT vector_extract(embedding) AS embedding_vector 
+        FROM movieB 
+        WHERE title = '${query}'
+      )
+      SELECT title 
+      FROM movieB, extracted_vector
+      ORDER BY vector_distance_cos(embedding, extracted_vector.embedding_vector) 
+      LIMIT 3
+    `);
+
+    console.timeEnd("Execution");
+
+    const data: MovieRe[] = rows.map((row: any) => ({
+      title: row.title,
+    }));
+
+    return data;
+  }
+
   return [];
 }
