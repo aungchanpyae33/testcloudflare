c2def15d3be697cccc25868e53ce3663b0270ab7
aungchanpyae <khantlu24@gmail.com>
2024-08-08 17:24:42 +0630

    create seekfunction for duplicate usage

--- src/ui/Footer/audio/AudioSeekBar.tsx
+++ src/ui/Footer/audio/AudioSeekBar.tsx
@@ -2,6 +2,7 @@ import DataContext from "@/lib/MediaSource/ContextMedia";
 import { playBackRate } from "@/lib/MediaSource/playBackRate";
 import { TimeFormat } from "@/lib/TimeFormat";
 import { useContext } from "react";
+import { eventProp } from "@/lib/MediaSource/playBackRate";
 function AudioSeekBar() {
   const {
     dataInput,
@@ -13,6 +14,14 @@ function AudioSeekBar() {
     loadNextSegment,
     segNum,
   } = useContext(DataContext);
+
+  function seekFunction(e: eventProp["e"]) {
+    if (!bottom) {
+      segNum.current = playBackRate({ dataAudio, e });
+      loadNextSegment();
+    }
+    setBottom(true);
+  }
   return (
     <input
       type="range"
@@ -20,23 +29,19 @@ function AudioSeekBar() {
       ref={dataInput}
       max={duration.durationTime}
       className="w-[50%]"
+      onKeyUp={(e) => {
+        seekFunction(e);
+      }}
       onMouseUp={(e) => {
-        if (!bottom) {
-          segNum.current = playBackRate({ dataAudio, e });
-          loadNextSegment();
-        }
-        setBottom(true);
+        seekFunction(e);
       }}
       onTouchEnd={(e) => {
-        if (!bottom) {
-          segNum.current = playBackRate({ dataAudio, e });
-          loadNextSegment();
-        }
-        setBottom(true);
+        seekFunction(e);
       }}
       // prevent timeupdate
       onTouchStart={() => setBottom(false)}
       onMouseDown={() => setBottom(false)}
+      onKeyDown={() => setBottom(false)}
       // show changeDuration while seeking without effecting the audio
       onInput={(e) => {
         dataCur.current!.textContent = TimeFormat(+e.currentTarget.value);
