233cb30937890a661e5a0c6adb76d20c6d1e53d2
aungchanpyae <khantlu24@gmail.com>
2024-08-08 17:20:35 +0630

    song container || playlist container

---
+++ src/ui/albumContainer/Container.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useRef, useState } from "react";
+import PlaylistContainer from "../playlist/PlaylistContainer";
+import ArrowNavi from "@/lib/Assibility/ArrowNavi";
+
+function Container({
+  songs,
+  description,
+}: {
+  songs: string[];
+  description: string;
+}) {
+  const numRefs = 7;
+  const dataInc = useRef(-1);
+  const refs = useRef<React.RefObject<HTMLDivElement>[]>(
+    Array.from({ length: numRefs }, () => React.createRef())
+  );
+
+  return (
+    <div
+      className="playlist border-2 overflow-auto  max-w-full"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        ArrowNavi(e, dataInc, refs);
+      }}
+    >
+      <h1
+        aria-label="song playlist name is playlist"
+        className="pl-4 sticky left-0"
+      >
+        playlist
+      </h1>
+
+      <div className="playlist w-fit gap-2   flex p-4">
+        {[...Array(7)].map((_, index) => (
+          <PlaylistContainer
+            key={index}
+            inputRef={refs.current[index]}
+            songs={songs}
+            description={index + description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Container;
---
+++ src/ui/playlist/PlaylistContainer.tsx
@@ -0,0 +1,35 @@
+import clsx from "clsx";
+import { index } from "drizzle-orm/mysql-core";
+import Image from "next/image";
+import Link from "next/link";
+
+interface prop {
+  songs: string[];
+  description: string;
+  inputRef: React.RefObject<HTMLDivElement>;
+}
+
+function PlaylistContainer({ songs, description, inputRef }: prop) {
+  return (
+    <div
+      tabIndex={-1}
+      ref={inputRef}
+      className={clsx(
+        "containerPlaylist p-3 md:p-4 lg:p-5       hover:bg-green-600 hover:shadow-lg rounded-md bg-green-500 shadow-md"
+      )}
+    >
+      <div className="flex imageContainer rounded-md w-[135px] md:w-[155px] lg:w-[175px] before:block before:pb-[100%]">
+        <Image
+          src="/ok.jpg"
+          width={100}
+          height={100}
+          alt="this is image element"
+          className="w-full h-full rounded-md"
+        />
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default PlaylistContainer;
