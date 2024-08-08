7e3689b6b53c6182a880ec40232fcce5aff6d3aa
aungchanpyae <khantlu24@gmail.com>
2024-08-08 17:22:45 +0630

    test nav link v0.1

--- src/ui/navtopBar/NavBar.tsx
+++ src/ui/navtopBar/NavBar.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import NavSideBar from "./NavSideBar";
-import clsx from "clsx";
+
 function NavBar() {
   return (
-    <nav className="navBarContainer flex w-full sticky top-0 bg-red-500  z-20 h-[10vh] items-center">
-      <Link href="/" className="Logo flex-1">
+    <nav className="navBarContainer flex w-full fixed  top-0  bg-red-500  z-20 h-[50px] items-center">
+      <div className="w-[50px]"></div>
+      <Link href="/" className="Logo flex-1 pl-2">
         Bubble
       </Link>
       <ul className="navLinkContainer w-[40%] flex flex-wrap   bg-green-500 justify-around">
@@ -12,16 +12,15 @@ function NavBar() {
           <a href="">About</a>
         </li>
         <li className="cursor-pointer">
-          <Link href={"/signup"}>sing up</Link>
+          <Link href={"/logout"}>sing out</Link>
         </li>
         <li className="cursor-pointer">
-          <Link href={"/signins"}>logout</Link>
+          <Link href={"/login"}>sign in</Link>
         </li>
         <li className="cursor-pointer">
           <a href="">github</a>
         </li>
       </ul>
-      <NavSideBar />
     </nav>
   );
 }
