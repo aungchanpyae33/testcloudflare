37f3588ba0debba214557fdc2ead7250a60be2c0
aungchanpyae <khantlu24@gmail.com>
2024-08-21 18:14:23 +0630

    remove SomeState v0.1

--- src/lib/MediaSource/ContextMedia.ts
+++ src/lib/MediaSource/ContextMedia.ts
@@ -1,33 +1,18 @@
 import React, { createContext, RefObject } from "react";
-import { PropTime } from "@/ui/Footer/AudioPLayer";
 export interface prop {
   dataAudio: React.MutableRefObject<HTMLAudioElement | null>;
-  setDuration: React.Dispatch<React.SetStateAction<PropTime>>;
-  bottom: boolean;
-  dataInput: React.MutableRefObject<HTMLInputElement | null>;
-  dataCur: React.MutableRefObject<HTMLSpanElement | null>;
-  duration: PropTime;
-  setBottom: React.Dispatch<React.SetStateAction<boolean>>;
+  duration: number;
+
   loadNextSegment: () => void;
   segNum: React.MutableRefObject<number>;
-  // play: boolean;
-  // setPlay: React.Dispatch<React.SetStateAction<boolean>>;
+  maxSegNum: number | undefined;
 }
-const defaultDuration: PropTime = {
-  cur: 0,
-  durationTime: undefined,
-};
+
 const DataContext = createContext<prop>({
   dataAudio: { current: null },
-  setDuration: () => {},
-  bottom: true,
-  dataInput: { current: null },
-  dataCur: { current: null },
   segNum: { current: 1 },
   loadNextSegment: () => {},
-  setBottom: () => {},
-  duration: defaultDuration,
-  // play: false,
-  // setPlay: () => {},
+  duration: 0,
+  maxSegNum: undefined,
 });
 export default DataContext;
