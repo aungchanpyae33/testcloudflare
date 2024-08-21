3acaf892b1a6318d8d46c46adcdd5d9d59661625
aungchanpyae <khantlu24@gmail.com>
2024-08-21 18:07:47 +0630

    Make own Component from AudioPLayer to avoid re-render

--- src/ui/Footer/audio/AudioElement.tsx
+++ src/ui/Footer/audio/AudioElement.tsx
@@ -1,27 +1,56 @@
 import { TimeFormat } from "@/lib/TimeFormat";
-import { useContext } from "react";
+import { ReactNode, useContext, useRef, useState } from "react";
 import DataContext from "@/lib/MediaSource/ContextMedia";
+import AudioSeekBar from "./AudioSeekBar";
+import TimeIndicatorCur from "./TimeIndicatorCur";
+export interface PropTime {
+  cur: number;
+  durationTime: number | undefined;
+}
+function AudioElement({
+  firstChild,
+  secondChild,
+}: {
+  firstChild: ReactNode;
+  secondChild: ReactNode;
+}) {
+  const dataInput = useRef<HTMLInputElement>(null);
 
-function AudioElement() {
-  const { dataAudio, setDuration, bottom, dataInput, dataCur } =
-    useContext(DataContext);
+  const [bottom, setBottom] = useState(true);
+  const dataCur = useRef<HTMLSpanElement>(null);
+  const { dataAudio, duration } = useContext(DataContext);
+  console.log("render audioELement");
   return (
-    <audio
-      hidden
-      ref={dataAudio}
-      autoPlay
-      onLoadedMetadata={() => {
-        setDuration({ cur: 0, durationTime: dataAudio.current?.duration });
-      }}
-      onTimeUpdate={(e) => {
-        if (bottom) {
-          dataInput.current!.value = e.currentTarget.currentTime.toString();
-          dataCur.current!.textContent = TimeFormat(
-            +e.currentTarget.currentTime
-          );
-        }
-      }}
-    ></audio>
+    <>
+      <audio
+        ref={dataAudio}
+        autoPlay
+        hidden
+        onLoadedMetadata={() => {
+          console.log(dataAudio.current?.duration);
+        }}
+        onTimeUpdate={(e) => {
+          if (bottom) {
+            dataInput.current!.value = e.currentTarget.currentTime.toString();
+            dataCur.current!.textContent = TimeFormat(
+              +e.currentTarget.currentTime
+            );
+          }
+        }}
+      ></audio>
+      <div className="flex justify-between">
+        {firstChild}
+        <TimeIndicatorCur dataCur={dataCur} />
+        <AudioSeekBar
+          dataCur={dataCur}
+          bottom={bottom}
+          setBottom={setBottom}
+          duration={duration}
+          dataInput={dataInput}
+        />
+        {secondChild}
+      </div>
+    </>
   );
 }
 
