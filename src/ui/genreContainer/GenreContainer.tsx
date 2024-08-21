82b57921c09ff3cb2fbd1dd9b9589401bb907f3b
aungchanpyae <khantlu24@gmail.com>
2024-08-21 17:42:35 +0630

    add Test IMAGE v0.1

--- src/ui/genreContainer/GenreContainer.tsx
+++ src/ui/genreContainer/GenreContainer.tsx
@@ -1,12 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 
 function GenreContainer({ description }: { description: string }) {
   return (
     <Link
       href={`genre/${description}`}
-      className="col-span-6 md:col-span-4 lg:col-span-3 bg-blue-600 h-[200px]"
+      className="col-span-6 md:col-span-4 lg:col-span-3 bg-blue-600 shrink-0 relative h-[200px]"
     >
-      {description}
+      <Image
+        src="https://s3.tebi.io/test1345/giraffe-8584272_640.png"
+        fill={true}
+        alt="this is image element"
+        className="w-full h-full rounded-md object-cover"
+      />
     </Link>
   );
 }
