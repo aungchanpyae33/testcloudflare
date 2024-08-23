e126596ee0d33bcb6e2d598169035f523f3102e3
aungchanpyae <khantlu24@gmail.com>
2024-08-23 16:57:27 +0630

    test use actual segeNum

--- src/app/(root)/album/[album]/page.tsx
+++ src/app/(root)/album/[album]/page.tsx
@@ -4,27 +4,27 @@ import Track from "@/ui/trackComponent/Track";
 const url = [
   {
     urlSong: "https://s3.tebi.io/test1345/new/init.mp4",
-    sege: 28,
+    sege: 27,
     duration: 52.199,
   },
   {
     urlSong: "https://s3.tebi.io/test1345/gd/init.mp4",
-    sege: 25,
+    sege: 24,
     duration: 232.896,
   },
   {
     urlSong: "https://s3.tebi.io/test1345/hello/init.mp4",
-    sege: 28,
+    sege: 27,
     duration: 52.199,
   },
   {
     urlSong: "https://s3.tebi.io/test1345/another/init.mp4",
-    sege: 37,
+    sege: 36,
     duration: 215.766,
   },
   {
     urlSong: " https://s3.tebi.io/test1345/some/init.mp4",
-    sege: 34,
+    sege: 33,
     duration: 226.155,
   },
 ];
