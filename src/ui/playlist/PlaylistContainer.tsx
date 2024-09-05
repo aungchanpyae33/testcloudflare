f4b75fcc2b9c5044489c1362b27e5f857b7dec72
aungchanpyae <khantlu24@gmail.com>
2024-09-05 10:07:47 +0630

    use RoleINdex for Dom Mani.. and add test image , avoid using ref for ovevrload v0.3

--- src/ui/playlist/PlaylistContainer.tsx
+++ src/ui/playlist/PlaylistContainer.tsx
@@ -5,24 +5,24 @@ import Link from "next/link";
 interface prop {
   songs: string[];
   description: string;
-  inputRef: React.RefObject<HTMLAnchorElement>;
+  index: number;
 }
 
-function PlaylistContainer({ songs, description, inputRef }: prop) {
+function PlaylistContainer({ songs, description, index }: prop) {
   return (
     <Link
       href={"album/supanova"}
       tabIndex={-1}
-      ref={inputRef}
+      role={`cell${index + 1}`}
       className={clsx(
-        "containerPlaylist p-3 md:p-4 lg:p-5       hover:bg-green-600 hover:shadow-lg rounded-md bg-green-500 shadow-md"
+        "containerPlaylist p-3 md:p-4 lg:p-5 hover:bg-green-600 hover:shadow-lg rounded-md bg-green-500 shadow-md"
       )}
     >
       <div className="flex imageContainer rounded-md w-[145px] md:w-[155px] lg:w-[175px] before:block before:pb-[100%]">
         <Image
-          src="/ok.jpg"
-          width={100}
-          height={100}
+          src="https://s3.tebi.io/test1345/timo-volz-ZlFKIG6dApg-unsplash%20%281%29.jpg"
+          width={300}
+          height={300}
           alt="this is image element"
           className="w-full h-full rounded-md"
         />
