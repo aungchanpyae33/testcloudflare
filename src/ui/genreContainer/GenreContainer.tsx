3d6c490faec4190532d58e61bab902e5a36a6614
aungchanpyae <khantlu24@gmail.com>
2024-09-05 10:09:29 +0630

    modifned style for test Image with 1:1 ration v0.2

--- src/ui/genreContainer/GenreContainer.tsx
+++ src/ui/genreContainer/GenreContainer.tsx
@@ -5,14 +5,19 @@ function GenreContainer({ description }: { description: string }) {
   return (
     <Link
       href={`genre/${description}`}
-      className="col-span-6 md:col-span-4 lg:col-span-3 bg-blue-600 shrink-0 relative h-[200px]"
+      className="col-span-6 md:col-span-4 lg:col-span-3 bg-blue-600 shrink-0 relative flex"
     >
       <Image
-        src="https://s3.tebi.io/test1345/giraffe-8584272_640.png"
-        fill={true}
+        src="https://s3.tebi.io/test1345/timo-volz-ZlFKIG6dApg-unsplash%20%281%29.jpg"
+        width={300}
+        height={300}
+        priority={false}
+        sizes="(min-width: 1040px) calc(25vw - 37px), (min-width: 780px) calc(33.33vw - 43px), calc(48.91vw - 47px)"
         alt="this is image element"
-        className="w-full h-full rounded-md object-cover"
+        className="object-cover w-[50%]"
       />
+
+      <p>{description}</p>
     </Link>
   );
 }
