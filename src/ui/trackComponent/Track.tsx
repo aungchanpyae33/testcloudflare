6c01abe1db74b229210f5540c20c4959effefb32
aungchanpyae <khantlu24@gmail.com>
2024-09-05 10:02:57 +0630

    Add accessbility for track block and add Duration prop v0.3

--- src/ui/trackComponent/Track.tsx
+++ src/ui/trackComponent/Track.tsx
@@ -1,37 +1,52 @@
 "use client";
-
 import { TimeFormat } from "@/lib/TimeFormat";
 import ToggleElement from "../Footer/audio/ToggleElement";
+import { FocusElement } from "@/lib/Assibility/FocusElement";
+import ArrowNavi from "@/lib/Assibility/ArrowNavi";
+import clsx from "clsx";
 
 function Track({
   name,
   duration,
-  item,
   url,
   sege,
+  index,
+  roleCell,
+  dataInc,
 }: {
   name: string;
   duration: number;
-  item: string;
   url: string;
   sege: number;
+  index: number;
+  roleCell: React.MutableRefObject<number>;
+  dataInc: React.MutableRefObject<number>;
 }) {
-  console.log(" render track");
-
   return (
     <div
-      className=" p-3 border border-black shadow-md mx-3 flex justify-between mb-2"
-      tabIndex={0}
-      // onKeyDown={(e) => {
-      //   if (e.key === "Enter") {
-      //     updateSong({ song_name: url });
-      //   }
-      // }}
+      className={clsx(
+        " p-3 border border-black shadow-md  flex justify-between mb-2 focus-within:bg-red-200 [&:has(:focus-visible)]:ring-4"
+      )}
+      tabIndex={-1}
+      id="uni1"
+      role={`cell${index + 1}`}
+      onKeyDown={(e) => {
+        ArrowNavi(e, roleCell, "ArrowRight", "ArrowLeft", 3, "rowCell");
+      }}
+      onFocus={(e) => {
+        dataInc.current = index + 1;
+        FocusElement(e.currentTarget, "rowCell", roleCell);
+      }}
     >
-      <p>{item}</p>
+      <button tabIndex={-1} role="rowCell1">
+        play
+      </button>
       <span>{name}</span>
       <span>{TimeFormat(duration)}</span>
       <ToggleElement url={url} sege={sege} duration={duration} />
+      <button tabIndex={-1} role="rowCell3">
+        hello
+      </button>
     </div>
   );
 }
