453b4981d263052ee371cae62330f5a1460c6be5
aungchanpyae <khantlu24@gmail.com>
2024-09-11 16:07:38 +0630

    remove lucia auth

--- src/app/auth/lucia.ts
+++
@@ -1,4 +0,0 @@
-import { db } from "@/database/turso";
-import { userTable, sessionTable } from "@/database/schema";
-import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
-export const adapter = new DrizzleSQLiteAdapter(db, sessionTable, userTable);
