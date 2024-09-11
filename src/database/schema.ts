d113a5b1829d3f50447333cee2cc168f7d4811dc
aungchanpyae <khantlu24@gmail.com>
2024-09-11 16:09:01 +0630

    remove lucia auth

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,16 +1,16 @@
-import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
+// import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
 
-export const userTable = sqliteTable("users", {
-  id: text("id").notNull().primaryKey(),
-  github_id: text("number").unique().notNull(),
-  username: text("string").notNull(),
-});
+// export const userTable = sqliteTable("users", {
+//   id: text("id").notNull().primaryKey(),
+//   github_id: text("number").unique().notNull(),
+//   username: text("string").notNull(),
+// });
 
-export const sessionTable = sqliteTable("session", {
-  id: text("id").notNull().primaryKey(),
-  userId: text("user_id")
-    .notNull()
-    .references(() => userTable.id),
-  expiresAt: integer("expires_at").notNull(),
-});
-export type CheckUser = typeof userTable.$inferInsert;
+// export const sessionTable = sqliteTable("session", {
+//   id: text("id").notNull().primaryKey(),
+//   userId: text("user_id")
+//     .notNull()
+//     .references(() => userTable.id),
+//   expiresAt: integer("expires_at").notNull(),
+// });
+// export type CheckUser = typeof userTable.$inferInsert;
