3744c5733aa1650e74ad4213c91c49630d6ba3b9
aungchanpyae <khantlu24@gmail.com>
2024-09-11 16:08:43 +0630

    remove lucia auth

--- src/database/turso.ts
+++ src/database/turso.ts
@@ -1,8 +1,6 @@
-import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 export const turso = createClient({
   url: "libsql://vectorsearch-ggop.turso.io",
   authToken:
     "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTgyODE2NTcsImlkIjoiY2I3MDZkZWItOWExZS00MGFlLTllMmYtYjFlZDhmMWI0YjYxIn0.Z06gSDEJGJHZ_wYw-CDbDCYd6Y8OFJStJueRQgXR9kSEUvOgnUv9C1mLmbrgPxSMUGdA-l7r-vnnxsaWJecXDA",
 });
-export const db = drizzle(turso);
