762baeeff07cb9c0a13f8821f85c049410bcb3ee
aungchanpyae <khantlu24@gmail.com>
2024-09-14 16:27:55 +0630

    remove lucia auth

--- src/app/(root)/playlist/page.tsx
+++ src/app/(root)/playlist/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { redirect } from "next/navigation";
-import { validateRequest } from "@/app/auth/auth";
 
 async function page() {
-  const { user } = await validateRequest();
-  if (!user) {
-    return redirect("/login");
-  }
-  return <h1>Hi, {user.username}!</h1>;
+  return <h1>explore</h1>;
 }
 
 export default page;
