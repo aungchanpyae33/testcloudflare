6ebb40ca17e48b09600888e00ad2b84f4dc927e5
aungchanpyae <khantlu24@gmail.com>
2024-09-17 01:12:59 +0630

    check stringLength in page

--- src/database/data.ts
+++ src/database/data.ts
@@ -2,27 +2,18 @@ import { turso } from "./turso";
 
 export interface Movie {
   title: string;
-
-  // Assuming you have a unique id for each movie
 }
 export interface MovieRe {
   title: string;
-
-  // Assuming you have a unique id for each movie
 }
 export const getData = async (query: string) => {
-  console.log(query);
-  if (query.length > 0) {
-    const { rows } = await turso.execute(
-      `SELECT title FROM movieB WHERE title LIKE '${query}%'`
-    );
-    const data: Movie[] = rows.map((row: any) => ({
-      title: row.title,
-    }));
-
-    return data;
-  }
-  return [];
+  const { rows } = await turso.execute(
+    `SELECT title FROM movieB WHERE title LIKE '${query}%'`
+  );
+  const data: Movie[] = rows.map((row: any) => ({
+    title: row.title,
+  }));
+  return data;
 };
 
 export async function getRecom(query: string) {
