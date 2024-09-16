2c8a364fb7c90f8481aba71ad0a2859deea0f2e1
aungchanpyae <khantlu24@gmail.com>
2024-09-17 01:17:33 +0630

    test ignore -

--- src/ui/navtopBar/NavBar.tsx
+++ src/ui/navtopBar/NavBar.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <nav className="navBarContainer flex w-full fixed  top-0  bg-red-500  z-20 h-[50px] items-center">
+    <nav className="navBarContainer flex w-full fixed  top-0  bg-red-500 z-10 h-[50px] items-center">
       <div className="w-[50px]"></div>
-      <Link href="/" className="Logo flex-1 pl-2">
-        Bubble
-      </Link>
+      <div className="logo flex-1 pl-2">
+        <Link href="/">Bubble</Link>
+      </div>
+
       <ul className="navLinkContainer w-[40%] flex flex-wrap   bg-green-500 justify-around">
         <li className="cursor-pointer">
           <a href="">About</a>
