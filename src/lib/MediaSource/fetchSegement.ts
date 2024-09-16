89bc649ed5689a4e32aa6b23ac2b31906e65eb4c
aungchanpyae <khantlu24@gmail.com>
2024-09-17 02:25:43 +0630

    add abort signale when current is pending

--- src/lib/MediaSource/fetchSegement.ts
+++ src/lib/MediaSource/fetchSegement.ts
@@ -3,13 +3,28 @@ import React from "react";
 export const fetchSegement = (
   url: string,
   sourceBuffer: React.MutableRefObject<SourceBuffer | null>,
-  segNum: number | undefined = undefined
+  segNum: number | undefined = undefined,
+  abortController: AbortController | null //need to get the data from other side ,so not use current
 ) => {
   const outputUrl = segNum ? url.replace("init.mp4", `seg-${segNum}.m4s`) : url;
-  console.log(outputUrl);
-  fetch(outputUrl)
-    .then((response) => response.arrayBuffer())
+  // console.log(outputUrl);
+  fetch(outputUrl, { signal: abortController!.signal })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`failed to fetch the song segements sege-${segNum}`);
+      }
+      return response.arrayBuffer();
+    })
     .then((buf) => {
-      sourceBuffer.current!.appendBuffer(buf);
+      if (sourceBuffer.current && !sourceBuffer.current.updating) {
+        sourceBuffer.current!.appendBuffer(buf);
+      }
+    })
+    .catch((err) => {
+      if (err.name === "AbortError") {
+        console.log(`the song segements sege-${segNum} fetching is aborted`);
+      } else {
+        console.error(`Error fetching segements sege-${segNum}`, err);
+      }
     });
 };
