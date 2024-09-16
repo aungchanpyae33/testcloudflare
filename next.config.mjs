924b3aced05d8c13cedcc4e5418c4531831dd4f9
aungchanpyae <khantlu24@gmail.com>
2024-09-16 17:47:13 +0630

    add test imaga config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.tebi.io",
+        port: "",
+        pathname: "/test1345/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
