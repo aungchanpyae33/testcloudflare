462fe7890a44c07104d5c840e33f3de27a4caeb1
aungchanpyae <khantlu24@gmail.com>
2024-09-17 01:14:16 +0630

    change maxSege to sege

--- src/lib/MediaSource/playBackRate.ts
+++ src/lib/MediaSource/playBackRate.ts
@@ -7,14 +7,14 @@ export interface eventProp {
 }
 interface prop extends eventProp {
   dataAudio: React.MutableRefObject<HTMLAudioElement | null>;
-  maxSegNum: number | undefined;
+  sege: number | undefined;
 }
-export function playBackRate({ dataAudio, e, maxSegNum }: prop) {
+export function playBackRate({ dataAudio, e, sege }: prop) {
   dataAudio.current!.currentTime = +e.currentTarget.value;
   const audioDu = dataAudio.current!.duration;
   const audioCu = dataAudio.current!.currentTime;
   const audioPosition = Math.floor((audioCu / audioDu) * 100);
-  const segPosition = Math.floor((maxSegNum! * audioPosition) / 100);
+  const segPosition = Math.floor((sege! * audioPosition) / 100);
 
   return segPosition;
 }
