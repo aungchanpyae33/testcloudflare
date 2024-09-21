cba709c344e3128fb05abfbca9b810ce0b9ece9b
aungchanpyae <khantlu24@gmail.com>
2024-09-21 18:58:57 +0630

    test

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,21 @@
+"use client";
 import Container from "@/ui/albumContainer/Container";
 
 async function page() {
+  console.log("hello");
   return (
     <div>
-      <audio
-        src="https://jolly-sun-bbad.bubblemusic990.workers.dev/api"
-        controls
-      ></audio>
+      <button
+        onClick={async () => {
+          const fetchData = await fetch(
+            "https://jolly-sun-bbad.bubblemusic990.workers.dev/api"
+          );
+          const data = await fetchData.json();
+          console.log(data);
+        }}
+      >
+        get data
+      </button>
       <Container songs={["hi"]} description="Hit Song of the week" />
     </div>
   );
