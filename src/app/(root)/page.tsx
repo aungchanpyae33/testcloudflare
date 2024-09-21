608d655bea3b53c8728ba3ff8375e2fbd66d5128
aungchanpyae <khantlu24@gmail.com>
2024-09-21 18:02:46 +0630

    test

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,10 @@ import Container from "@/ui/albumContainer/Container";
 async function page() {
   return (
     <div>
-      <audio src="/api" controls></audio>
+      <audio
+        src="https://jolly-sun-bbad.bubblemusic990.workers.dev/api"
+        controls
+      ></audio>
       <Container songs={["hi"]} description="Hit Song of the week" />
     </div>
   );
