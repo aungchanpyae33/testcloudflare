5c56ef12c91a24343a411794fe7cfdf73b6df8e8
aungchanpyae <khantlu24@gmail.com>
2024-09-23 01:42:05 +0630

    test

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,4 @@
+export const runtime = "edge";
 import { type NextRequest } from "next/server";
 import { getRequestContext } from "@cloudflare/next-on-pages";
 export async function GET(request: NextRequest) {
