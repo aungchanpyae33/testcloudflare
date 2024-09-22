365ec873ab6853b08d500939b4eac1705036ace2
aungchanpyae <khantlu24@gmail.com>
2024-09-23 01:38:31 +0630

    test

---
+++ src/app/api/route.ts
@@ -0,0 +1,15 @@
+import { type NextRequest } from "next/server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query: string | null = searchParams.get("q");
+  if (query) {
+    const fetchData = await fetch(query, {
+      cf: {
+        cacheTtl: 36000,
+        cacheEverything: true,
+      },
+    });
+    return fetchData;
+  }
+}
--- src/lib/MediaSource/fetchSegement.ts
+++ src/lib/MediaSource/fetchSegement.ts
@@ -1,26 +1,18 @@
 import React from "react";
-import { getRequestContext } from "@cloudflare/next-on-pages";
+
 export const fetchSegement = (
   url: string,
   sourceBuffer: React.MutableRefObject<SourceBuffer | null>,
   segNum: number | undefined = undefined,
   abortController: AbortController | null //need to get the data from other side ,so not use current
 ) => {
-  const { cf } = getRequestContext();
   const outputUrl = segNum ? url.replace("init.mp4", `seg-${segNum}.m4s`) : url;
   // console.log(outputUrl);
-  fetch(outputUrl, {
-    signal: abortController!.signal,
-    cf: {
-      cacheTtl: 36000,
-      cacheEverything: true,
-    },
-  })
+  fetch(`/api/?q=${outputUrl}`, { signal: abortController!.signal })
     .then((response) => {
       if (!response.ok) {
         throw new Error(`failed to fetch the song segements sege-${segNum}`);
       }
-
       return response.arrayBuffer();
     })
     .then((buf) => {
