3c0a58e14fdf817a04fab358ee810f9d7851a309
aungchanpyae <khantlu24@gmail.com>
2024-09-24 21:31:04 +0630

    g

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -4,14 +4,16 @@ import { getRequestContext } from "@cloudflare/next-on-pages";
 
 export async function GET(request: NextRequest) {
   const query: string | null = "https://words.dev-apis.com/word-of-the-day";
-
   const fetchData = await fetch(query, {
     cf: {
       cacheTtl: 36000,
       cacheEverything: true,
     },
   });
-  const response = new NextResponse(fetchData.body);
+  const response = new NextResponse(fetchData.body, {
+    status: fetchData.status,
+    headers: fetchData.headers,
+  });
   response.headers.set("Cloudflare-CDN-Cache-Control", "public,max-age=36000");
   return response;
 }
