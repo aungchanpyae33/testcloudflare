b0ef04e0bdf2c5b00dcca4b320dfbf129cc87f0d
AungChnaPyae <138773319+aungchanpyae33@users.noreply.github.com>
2025-01-25 11:44:56 +0630

    Update page.tsx

--- src/app/(root)/album/[album]/page.tsx
+++ src/app/(root)/album/[album]/page.tsx
@@ -4,14 +4,14 @@ import AudiosContainer from "@/ui/albumContainer/Album";
 export const runtime = "edge";
 const url = [
 {
-    url: "https://tebi.bubblemusic.us.kg/init.mp4",
+    urlSong: "https://tebi.bubblemusic.us.kg/init.mp4",
     sege: 24,
     name: "gonenewtest",
     duration: 239.467,
   },
 
   {
-    url: "https://tebi.bubblemusic.us.kg/bob/init.mp4",
+    urlSong: "https://tebi.bubblemusic.us.kg/bob/init.mp4",
     sege: 24,
     name: "paper lady",
     duration: 239.467,
